61654080768b338b5da3243249f24440be8e3321
OnionSm <vuongha16104@gmail.com>
2024-07-11 22:01:04 +0700

    random card, click card

---
+++ Memory Matching/Assets/CardScripts/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private Cards card_configs;
+
+    public static GameManager Instance;
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Lấy config của tất cả card
+    public Cards GetCardConfigs()
+    {
+        return card_configs;
+    }
+
+}
